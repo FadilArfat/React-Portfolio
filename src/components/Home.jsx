e8f3e4532b2399c9a085ee7a5c75384c38660753
FadilArfat <fadilitas14@gmail.com>
2023-05-06 10:26:33 +0700

    optimisasi seo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,34 +2,45 @@ import React, { useEffect, useRef } from "react";
 import HeroImage from "../assets/header_head.png";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
-import Typed from 'typed.js'
-import styles from './Home.module.css'
+import Typed from "typed.js";
+import styles from "./Home.module.css";
 
 const Home = () => {
-  const el = useRef(null)
+  const el = useRef(null);
 
   useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ['Coder','Designer', 'Freelancer'],
+      strings: ["Coder", "Designer", "Freelancer"],
       typeSpeed: 50,
-      loop: true
+      loop: true,
     });
 
     return () => {
       // Destroy Typed instance during cleanup to stop animation
       typed.destroy();
     };
-  }, [])
+  }, []);
 
   return (
     <div name="home" className="h-screen w-full mb-5 md:mb-0 sm:mb-72">
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center max-h-max pt-28 md:pt-0 md:h-full px-4 md:flex-row">
         <div className="w-full relative">
           <p className="text-gray-500 py-4 max-w-md font-extrabold">Hello</p>
-          <h2 className="outline-title text-4xl sm:text-7xl font-bold text-white">I'm <span ref={el} /></h2>
-          <p className="text-gray-500 py-4 max-w-md">Freshgraduate from computer science. Currently, I love to work on web application using technologies like React, Tailwind, and Bootstrap.</p>
+          <h2 className="outline-title text-4xl sm:text-7xl font-bold text-white">
+            I'm <span ref={el} />
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
+            Freshgraduate from computer science. Currently, I love to work on
+            web application using technologies like React, React Native, NextJS,
+            Tailwind, and Bootstrap.
+          </p>
           <div>
-            <Link to="portfolio" smooth duration={500} className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-blue-500 to-blue-800 cursor-pointer">
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-blue-500 to-blue-800 cursor-pointer"
+            >
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
                 <MdKeyboardArrowRight size={25} className="ml-1" />
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,53 +5,63 @@ import { db } from "../api/firebase";
 
 const Portfolio = () => {
   const [isi, setIsi] = useState([]);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   console.log(isi);
 
   useEffect(() => {
     const ambilData = async () => {
       try {
-        const getDataFromFB = []
-      const docRef = collection(db, "projects");
-      const docSnap = await getDocs(docRef);
-      docSnap.forEach(doc => {
-        getDataFromFB.push({
-          ...doc.data(),
-          key: doc.id,
-        })
-})
-setIsi(getDataFromFB);
-setLoading(false)
-      
+        const getDataFromFB = [];
+        const docRef = collection(db, "projects");
+        const docSnap = await getDocs(docRef);
+        docSnap.forEach((doc) => {
+          getDataFromFB.push({
+            ...doc.data(),
+            key: doc.id,
+          });
+        });
+        setIsi(getDataFromFB);
+        setLoading(false);
       } catch (error) {
-        console.log(error)
-        setLoading(true)
+        console.log(error);
+        setLoading(true);
       }
-     
-    }
+    };
     ambilData();
-  },[])
+  }, []);
 
   return (
-    <div name="portfolio" className="mt-10 pb-24 md:pb-5  w-full text-white h-full md:h-screen">
+    <div
+      name="portfolio"
+      className="mt-10 pb-24 md:pb-5  w-full text-white h-full md:h-screen"
+    >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div data-aos="fade-right" className="pb-5">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-        {loading ? <h1>Loading...</h1> : isi.map(({ image, key, link }) => (
-            <div key={key} className="shadow-md shadow-gray-600 rounded-lg">
-              <img src={image} alt="" className={`rounded-md mx-auto duration-200 hover:scale-105 `} />
+          {loading ? (
+            <h1>Loading...</h1>
+          ) : (
+            isi.map(({ image, key, link }) => (
+              <div key={key} className="shadow-md shadow-gray-600 rounded-lg">
+                <img
+                  src={image}
+                  alt=""
+                  className={`rounded-md mx-auto duration-200 hover:scale-105 `}
+                />
 
-              <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  <a href={link}> Check it out</a>
-                </button>
+                <div className="flex items-center justify-center">
+                  <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                    <a href={link}> Check it out</a>
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
-          
+            ))
+          )}
         </div>
       </div>
     </div>
